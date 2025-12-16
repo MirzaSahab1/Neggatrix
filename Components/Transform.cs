b37ee6a94467dc433acd03f82ac0ed1f35342298
unknown <talalmughal996@gmail.com>
2025-12-16 22:32:15 +0500

    Added: Transform Component

---
+++ Components/Transform.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neggatrix.Core;
+using Neggatrix.Interfaces;
+
+namespace Neggatrix.Components
+{
+    public class Transform : IComponent
+    {
+        public required GameObject Owner {  get; set; }
+
+        public PointF Position { get; set; } = new PointF(0.0f, 0.0f);
+
+        public PointF Pivot { get; set; } = new PointF(0.5f, 0.5f);
+        public float Rotation { get; set; }
+
+        public PointF Scale { get; set; } = new PointF(1f, 1f);
+
+       
+        public void Start() { }
+        public void Update() { }
+        public void Destroy() { }
+    }
+}
--- obj/Debug/net8.0-windows/Neggatrix.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Neggatrix.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Neggatrix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee6a52d0fed3fb21ab023d615db74e358259d9b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36c9f1f7881b7eac6b67d6378a1db50208334366")]
 [assembly: System.Reflection.AssemblyProductAttribute("Neggatrix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Neggatrix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
